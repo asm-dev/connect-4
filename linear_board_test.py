2d8f2f0e61a28160f31851130b704a7e13c2eba8
asm-dev <salamanca.m@outlook.com>
2022-10-09 17:54:19 +0200

    linear board

---
+++ linear_board_test.py
@@ -0,0 +1,30 @@
+import pytest
+from linear_board import *
+from settings import BOARD_LENGHT, VICTORY_STRIKE
+
+def test_empty_board():
+    empty = LinearBoard()
+    assert empty != None
+    assert empty.is_full() == False
+    assert empty.is_victory('x') == False
+
+def test_add():
+    board = LinearBoard()
+    for i in range(BOARD_LENGHT):
+        board.add('x')
+    assert board.is_full() # == True
+
+def test_victory():
+    board = LinearBoard()
+    for i in range(VICTORY_STRIKE):
+        board.add('x')
+    assert board.is_victory('o') == False
+    assert board.is_victory('x') # == True
+
+def test_tie():
+    board = LinearBoard()
+    board.add('o')
+    board.add('o')
+    board.add('x')
+    board.add('o')
+    assert board.is_tie('x', 'o') # == True
---
+++ settings.py
@@ -0,0 +1,3 @@
+# We determine the lenght of our board and the victory strike (number of connected elements) in order to win.
+BOARD_LENGHT = 4
+VICTORY_STRIKE = 3
